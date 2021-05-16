b30ef9b75ae580d5aa8d2c64280154c7f4c429f9
XtraS001 <xushengchin@gmail.com>
2021-05-16 22:29:55 +0800

    XS latest adj list 16_5_2021

--- adj_list_XS.py
+++ adj_list_XS.py
@@ -1,26 +1,30 @@
 
 class Graph:
-    def __init__(self, Nodes, is_directed = False):
-        self.nodes = Nodes
+    def __init__(self, Nodes, is_directed=False):
+        self.nodes = Nodes  # A,B,C,D
         self.adj_list = {}
+        self.weight = {}
         self.is_directed = is_directed
 
         for node in self.nodes:
-            self.adj_list[node] = []
+            self.adj_list[node] = []    #Create array to store Destination for that node
+            self.weight[node] = []      #Create array to store weight
 
-    def add_edge(self, u, v):
+    def add_edge(self, u, v, w):
         self.adj_list[u].append(v)
+        self.weight[u].append(w)
 
         if not self.is_directed:
             self.adj_list[v].append(u)
+            self.weight[u].append(w)
 
-    def del_edge(self, u, v):
+    def del_edge(self, u, v, w):
         self.adj_list[u].remove(v)
+        self.weight[u].remove(w)
 
         if not self.is_directed:
             self.adj_list[v].remove(u)
-
-
+            self.weight[v].remove(w)
 
     def degree(self, node):
         deg = len(self.adj_list[node])
@@ -28,10 +32,11 @@ class Graph:
 
     def print_adj_list(self):
         for node in self.nodes:
-            print(node, "->", self.adj_list[node])
+            print("List  : ", node, "->", self.adj_list[node])
+            print("Weight: ", node, "->", self.weight[node])
 
 all_edges = {
-    ("A", "B"), ("A", "C"), ("B", "D"), ("C", "D"), ("C", "E"), ("D", "E")
+    ("A", "B", 1), ("A", "C", 2), ("B", "D", 3), ("C", "D", 4), ("C", "E", 5), ("D", "E", 6)
 }
 
 
@@ -39,7 +44,7 @@ nodes = ["A", "B", "C", "D", "E"]
 graph = Graph(nodes, True)
 # graph.print_adj_list()
 
-for u ,v in all_edges:
-    graph.add_edge(u ,v)
+for u, v, w in all_edges:
+    graph.add_edge(u, v, w)
 
 graph.print_adj_list()
